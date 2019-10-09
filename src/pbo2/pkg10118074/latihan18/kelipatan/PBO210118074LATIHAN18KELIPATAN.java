5ca0c956a86bdcfbcfccd120fdef97666e653ca8
fikrimaualana <55828126+Fikrimaulana15@users.noreply.github.com>
2019-10-09 20:41:17 +0700

    SELESAI

---
+++ src/pbo2/pkg10118074/latihan18/kelipatan/PBO210118074LATIHAN18KELIPATAN.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118074.latihan18.kelipatan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO210118074LATIHAN18KELIPATAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      double i;
+      for(i = 3.5 ; i<=35; i+=3.5)
+        System.out.println(i);
+    
+    }
+    
+}
